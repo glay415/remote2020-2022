b8d6c35ce1c9317b386a158e47f79afedf0e01d9
김범진 <sdpthf@naver.com>
2021-08-08 18:37:28 +0900

    SecurityConfig

---
+++ movie/src/main/java/com/example/project/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.project.config;
+
+import com.example.project.config.jwt.JwtAuthenticationFilter;
+import com.example.project.config.jwt.JwtAuthorizationFilter;
+import com.example.project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsConfig corsConfig;
+    private final UserRepository userRepository;
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .addFilter(corsConfig.corsFilter())
+                .csrf().disable();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
+
+                .authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+    }
+}
