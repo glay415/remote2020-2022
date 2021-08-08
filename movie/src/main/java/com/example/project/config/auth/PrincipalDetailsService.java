0c02a7a5d8cb411fabdaa0b392445cb926657b5e
김범진 <sdpthf@naver.com>
2021-08-08 18:36:21 +0900

    PrincipalDetailsService

---
+++ movie/src/main/java/com/example/project/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.project.config.auth;
+
+import com.example.project.model.User;
+import com.example.project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("PrincipalDetailsService : 진입");
+        User user = userRepository.findByUsername(username);
+
+        // session.setAttribute("loginUser", user);
+        return new PrincipalDetails(user);
+    }
+}
