2173883ca0c8b1c5cecae48a882be7e193376c5a
김범진 <sdpthf@naver.com>
2021-08-11 19:36:38 +0900

    PrincipalDetailsService

--- SupportPing/src/main/java/com/example/project/config/auth/PrincipalDetailsService.java
+++ SupportPing/src/main/java/com/example/project/config/auth/PrincipalDetailsService.java
@@ -16,10 +16,8 @@ public class PrincipalDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("PrincipalDetailsService : 진입");
         User user = userRepository.findByUsername(username);
 
-        // session.setAttribute("loginUser", user);
         return new PrincipalDetails(user);
     }
 }
