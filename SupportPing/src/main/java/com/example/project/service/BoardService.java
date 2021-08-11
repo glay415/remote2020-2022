8b8754ad7022c38a3382fffae37c0758a253bd38
김범진 <sdpthf@naver.com>
2021-08-11 20:17:34 +0900

    BoardService

--- SupportPing/src/main/java/com/example/project/service/BoardService.java
+++ SupportPing/src/main/java/com/example/project/service/BoardService.java
@@ -8,6 +8,7 @@ import com.example.project.domain.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
