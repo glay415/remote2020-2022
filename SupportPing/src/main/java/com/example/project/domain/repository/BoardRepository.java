d30eb4ee6e39a2645e3cf9d0cadac7ebd6ad205e
김범진 <sdpthf@naver.com>
2021-08-13 21:27:48 +0900

    BoardRepository

--- SupportPing/src/main/java/com/example/project/domain/repository/BoardRepository.java
+++ SupportPing/src/main/java/com/example/project/domain/repository/BoardRepository.java
@@ -2,7 +2,6 @@ package com.example.project.domain.repository;
 
 import com.example.project.domain.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
