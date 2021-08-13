4330c570868bee683a103dfa734a71bc8de8d08f
김범진 <sdpthf@naver.com>
2021-08-13 21:29:36 +0900

    ParticipantsAlreadyExceedException

--- SupportPing/src/main/java/com/example/project/exception/ParticipantsAlreadyExceedException.java
+++ SupportPing/src/main/java/com/example/project/exception/ParticipantsAlreadyExceedException.java
@@ -2,7 +2,7 @@ package com.example.project.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class ParticipantsAlreadyExceedException extends BusinessException {
+public class ParticipantsAlreadyExceedException extends BusinessException{
     public ParticipantsAlreadyExceedException() {
         super("PARTICIPANTS_ALREADY_EXCEED", HttpStatus.NOT_ACCEPTABLE);
     }
