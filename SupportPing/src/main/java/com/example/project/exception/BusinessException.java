d47522b55cb243c0277504b068d0cdecba4e9bc1
김범진 <sdpthf@naver.com>
2021-08-13 21:28:10 +0900

    BusinessException

--- SupportPing/src/main/java/com/example/project/exception/BusinessException.java
+++ SupportPing/src/main/java/com/example/project/exception/BusinessException.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public class BusinessException extends RuntimeException {
+public class BusinessException extends RuntimeException{
     private final String code;
     private final HttpStatus status;
 }
