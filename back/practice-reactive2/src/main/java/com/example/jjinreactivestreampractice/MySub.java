89e0fe7a8a6a9ddfda8a594958684fcebc4774f5
이준서 <ljchan71@gmail.com>
2021-08-26 20:25:11 +0900

    today-study

--- back/practice-reactive2/src/main/java/com/example/jjinreactivestreampractice/MySub.java
+++ back/practice-reactive2/src/main/java/com/example/jjinreactivestreampractice/MySub.java
@@ -6,18 +6,25 @@ import org.reactivestreams.Subscription;
 public class MySub implements Subscriber<Integer> {
 
     private Subscription s;
+    private int bufferSize = 2;
 
     @Override
     public void onSubscribe(Subscription s) {
         System.out.println("구독자 : 구독 정보 받았어");
         this.s = s;
         System.out.println("구독자:나 이제 신문 1개씩 줘");
-        s.request(12);
+        s.request(bufferSize);
     }
 
     @Override
     public void onNext(Integer t) {
         System.out.println("onNext() :"+ t);
+        bufferSize--;
+        if(bufferSize == 0){
+            System.out.println("하루 지남");
+            bufferSize=2;
+            s.request(bufferSize);
+        }
     }
 
     @Override
