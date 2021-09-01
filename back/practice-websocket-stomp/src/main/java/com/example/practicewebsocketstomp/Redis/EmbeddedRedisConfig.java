943c6418fe17043f549cc078b2b22fbcb8f52e32
이준서 <ljchan71@gmail.com>
2021-09-01 13:43:34 +0900

    websocket-stomp:RedisConfig

---
+++ back/practice-websocket-stomp/src/main/java/com/example/practicewebsocketstomp/Redis/EmbeddedRedisConfig.java
@@ -0,0 +1,29 @@
+package com.example.practicewebsocketstomp.Redis;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Profile("local")
+@Configuration
+public class EmbeddedRedisConfig {
+
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null)
+            redisServer.stop();
+    }
+}
