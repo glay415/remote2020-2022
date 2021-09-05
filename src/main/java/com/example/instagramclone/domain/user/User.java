e0e3579af88074bbe54b895100195403278bf56d
이준서 <ljchan71@gmail.com>
2021-09-05 14:29:51 +0900

    add::UserEntity

---
+++ src/main/java/com/example/instagramclone/domain/user/User.java
@@ -0,0 +1,36 @@
+package com.example.instagramclone.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+    private String name;
+    private String bio;
+    private String email;
+    private String phone;
+    private String gender;
+
+    private String profileImageUrl;
+    private String role;
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    private void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+}
