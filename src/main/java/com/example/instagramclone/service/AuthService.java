851271b3b6f709e753e31786671be9ba927f097f
이준서 <ljchan71@gmail.com>
2021-09-05 14:35:31 +0900

    add::AuthService

---
+++ src/main/java/com/example/instagramclone/service/AuthService.java
@@ -0,0 +1,14 @@
+package com.example.instagramclone.service;
+
+import com.example.instagramclone.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+
+    public void join(User user) {
+
+    }
+}
