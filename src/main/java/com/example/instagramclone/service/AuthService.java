547d81eba918a1548045fd77c4638ca33c1a5443
이준서 <ljchan71@gmail.com>
2021-09-05 14:57:49 +0900

    add::AuthService
    toEntity logic / hash password / role / Transactional

--- src/main/java/com/example/instagramclone/service/AuthService.java
+++ src/main/java/com/example/instagramclone/service/AuthService.java
@@ -2,16 +2,30 @@ package com.example.instagramclone.service;
 
 import com.example.instagramclone.domain.user.User;
 import com.example.instagramclone.domain.user.UserRepository;
+import com.example.instagramclone.web.dto.auth.JoinDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @RequiredArgsConstructor
 @Service
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public User join(User user) {
+    @Transactional
+    public User join(JoinDto joinDto) {
+        String encPassword = bCryptPasswordEncoder.encode(joinDto.getPassword());
+        User user = User.builder()
+                .username(joinDto.getUsername())
+                .password(encPassword)
+                .email(joinDto.getEmail())
+                .name(joinDto.getName())
+                .role("ROLE_USER")
+                .build();
         User userEntity = userRepository.save(user);
         return userEntity;
     }
