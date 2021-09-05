85b313ea9809557ae38c84fb230fda72b7d01799
이준서 <ljchan71@gmail.com>
2021-09-05 16:16:55 +0900

    Make::ControllerExceptionHandler
    for client response

--- src/main/java/com/example/instagramclone/handler/ControllerExceptionHandler.java
+++ src/main/java/com/example/instagramclone/handler/ControllerExceptionHandler.java
@@ -1,19 +1,21 @@
 package com.example.instagramclone.handler;
 
 import com.example.instagramclone.handler.ex.CustomValidationException;
-import com.example.instagramclone.web.dto.CMRespDto;
+import com.example.instagramclone.utill.Script;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public CMRespDto<?> validationException(CustomValidationException e) {
-        return new CMRespDto<Map<String,String>>(-1, e.getMessage(), e.getErrorMap());
+    public String validationException(CustomValidationException e) {
+        return Script.back(e.getErrorMap().toString());
     }
+    
+//    public new CMRespDto<?> validationException(CustomValidationException e) {
+//        return new CMRespDto<Map<String, String>>(-1,e.getMessage(),e.getErrorMap());
+//    }
 }
