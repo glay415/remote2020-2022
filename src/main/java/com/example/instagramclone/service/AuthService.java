a07f22720380e657bdfb0906b7a2169b5cb84848
이준서 <ljchan71@gmail.com>
2021-09-05 14:42:03 +0900

    add:AuthService
    join logic

--- src/main/java/com/example/instagramclone/service/AuthService.java
+++ src/main/java/com/example/instagramclone/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.example.instagramclone.service;
 
 import com.example.instagramclone.domain.user.User;
+import com.example.instagramclone.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthService {
 
-    public void join(User user) {
+    private final UserRepository userRepository;
 
+    public User join(User user) {
+        User userEntity = userRepository.save(user);
+        return userEntity;
     }
 }
