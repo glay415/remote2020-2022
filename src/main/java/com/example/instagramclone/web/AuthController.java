569db00d54a4b67bc916199212b210c4d2b08a44
이준서 <ljchan71@gmail.com>
2021-09-05 14:42:43 +0900

    add:AuthController
    join logic

--- src/main/java/com/example/instagramclone/web/AuthController.java
+++ src/main/java/com/example/instagramclone/web/AuthController.java
@@ -1,12 +1,18 @@
 package com.example.instagramclone.web;
 
+import com.example.instagramclone.domain.user.User;
+import com.example.instagramclone.service.AuthService;
 import com.example.instagramclone.web.dto.auth.JoinDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class AuthController {
+
+    private final AuthService authService;
 
     @GetMapping("/auth/join")
     public String joinForm() {
@@ -15,6 +21,9 @@ public class AuthController {
 
     @PostMapping("/auth/join")
     public String join(JoinDto joinDto){
+        User user = joinDto.toEntity();
+        User userEntity = authService.join(user);
+        System.out.println(userEntity);
         return "auth/Login";
     }
 
