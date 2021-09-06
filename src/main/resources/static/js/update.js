cdc99a4534371da31551581a650884aef272aa61
이준서 <ljchan71@gmail.com>
2021-09-06 22:14:29 +0900

    add::update.js

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,4 +1,20 @@
 // (1) 회원정보 수정
-function update() {
+function update(userId) {
+    event.preventDefault();
+    let data = $("#profileUpdate").serialize();
 
+    console.log(data);
+
+    $.ajax({
+        type: "put",
+        url : `/api/user/${userId}`,
+        data: data,
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+        dataType: "json"
+    }).done(res=>{
+        console.log("성공", res);
+        location.href = `/user/${userId}`;
+    }).fail(error=>{
+        alert(JSON.stringify(error.responseJSON.data));
+    });
 }
