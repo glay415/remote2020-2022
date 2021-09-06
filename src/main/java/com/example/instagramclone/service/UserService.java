b202cdeaf55bbe4d0b807c506a8129d2bfae215f
이준서 <ljchan71@gmail.com>
2021-09-06 22:16:36 +0900

    add::UserService
    updateUser logic

---
+++ src/main/java/com/example/instagramclone/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.instagramclone.service;
+
+import com.example.instagramclone.domain.user.User;
+import com.example.instagramclone.domain.user.UserRepository;
+import com.example.instagramclone.web.dto.user.UserUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public User updateUser(int id, User user) {
+
+        User userEntity = userRepository.findById(id).get();
+
+        userEntity.setName(user.getName());
+        userEntity.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userEntity.setBio(user.getBio());
+        userEntity.setWebsite(user.getWebsite());
+        userEntity.setPhone(user.getPhone());
+        userEntity.setGender(user.getGender());
+        return userEntity;
+    }
+}
