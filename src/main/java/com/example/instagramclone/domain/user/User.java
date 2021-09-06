f88f5856400fe35fdd71d6e9547d13034e2397b6
이준서 <ljchan71@gmail.com>
2021-09-06 22:21:06 +0900

    add::UserEntity
    setter

--- src/main/java/com/example/instagramclone/domain/user/User.java
+++ src/main/java/com/example/instagramclone/domain/user/User.java
@@ -1,9 +1,6 @@
 package com.example.instagramclone.domain.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -12,6 +9,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @Entity
 public class User {
 
