e7f206d481ebf3174f9bb10d716379aa1b5ccd7a
이준서 <ljchan71@gmail.com>
2021-09-07 12:55:54 +0900

    Avoid::UserService
    orElseThrow

--- src/main/java/com/example/instagramclone/service/UserService.java
+++ src/main/java/com/example/instagramclone/service/UserService.java
@@ -2,11 +2,14 @@ package com.example.instagramclone.service;
 
 import com.example.instagramclone.domain.user.User;
 import com.example.instagramclone.domain.user.UserRepository;
+import com.example.instagramclone.handler.ex.CustomValidationApiException;
 import com.example.instagramclone.web.dto.user.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Supplier;
 
 @RequiredArgsConstructor
 @Service
@@ -18,7 +21,7 @@ public class UserService {
     @Transactional
     public User updateUser(int id, User user) {
 
-        User userEntity = userRepository.findById(id).get();
+        User userEntity = userRepository.findById(id).orElseThrow(()->{ return new CustomValidationApiException("찾을 수 없는 ID 입니다");});
 
         userEntity.setName(user.getName());
         userEntity.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
