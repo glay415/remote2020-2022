38b414190dc108e3b6dcfa0726cb591ce0dae40f
이준서 <ljchan71@gmail.com>
2021-09-07 23:23:14 +0900

    avoid::ImageController
    file empty

--- src/main/java/com/example/instagramclone/web/ImageController.java
+++ src/main/java/com/example/instagramclone/web/ImageController.java
@@ -1,6 +1,7 @@
 package com.example.instagramclone.web;
 
 import com.example.instagramclone.config.auth.PrincipalDetails;
+import com.example.instagramclone.handler.ex.CustomValidationException;
 import com.example.instagramclone.service.ImageService;
 import com.example.instagramclone.web.dto.image.ImageUploadDto;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +34,11 @@ public class ImageController {
 
     @PostMapping("/image")
     public String imageUpload(ImageUploadDto imageUploadDto, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        if(imageUploadDto.getFile().isEmpty()) {
+            throw new CustomValidationException("이미지가 첨부되지 않았습니다.", null);
+        }
+
         imageService.imageUpload(imageUploadDto, principalDetails);
         return "redirect:/user/"+principalDetails.getUser().getId();
     }
