dc48a74ea5a8cb5a068fba241b7360ff99cd10c8
이준서 <ljchan71@gmail.com>
2021-09-07 23:23:30 +0900

    add::ImageUploadDto
    toEntity

--- src/main/java/com/example/instagramclone/web/dto/image/ImageUploadDto.java
+++ src/main/java/com/example/instagramclone/web/dto/image/ImageUploadDto.java
@@ -1,5 +1,7 @@
 package com.example.instagramclone.web.dto.image;
 
+import com.example.instagramclone.domain.image.Image;
+import com.example.instagramclone.domain.user.User;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,4 +9,12 @@ import org.springframework.web.multipart.MultipartFile;
 public class ImageUploadDto {
     private MultipartFile file;
     private String cation;
+
+    public Image toEntity(String postImageUrl, User user) {
+        return Image.builder()
+                .caption(cation)
+                .postImageUrl(postImageUrl)
+                .user(user)
+                .build();
+    }
 }
