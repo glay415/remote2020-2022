f6eba963e162b96324df4b0f3544f4452cbed7fd
이준서 <ljchan71@gmail.com>
2021-09-07 22:52:41 +0900

    add::ImageService

---
+++ src/main/java/com/example/instagramclone/service/ImageService.java
@@ -0,0 +1,40 @@
+package com.example.instagramclone.service;
+
+import com.example.instagramclone.config.auth.PrincipalDetails;
+import com.example.instagramclone.domain.image.Image;
+import com.example.instagramclone.domain.image.ImageRepository;
+import com.example.instagramclone.web.dto.image.ImageUploadDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    public void imageUpload(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+        UUID uuid = UUID.randomUUID();
+        String imageFileName = uuid + "_" + imageUploadDto.getFile().getOriginalFilename();
+        System.out.println("이미지 파일이름"+imageFileName);
+
+        Path imageFilePath = Paths.get(uploadFolder + imageFileName);
+
+        try {
+            Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        
+    }
+}
