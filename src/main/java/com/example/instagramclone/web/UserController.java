af1538f63f27626fdcf6078132aa42990cd37a32
이준서 <ljchan71@gmail.com>
2021-09-12 20:21:25 +0900

    add::UserController

--- src/main/java/com/example/instagramclone/web/UserController.java
+++ src/main/java/com/example/instagramclone/web/UserController.java
@@ -3,6 +3,7 @@ package com.example.instagramclone.web;
 import com.example.instagramclone.config.auth.PrincipalDetails;
 import com.example.instagramclone.domain.user.User;
 import com.example.instagramclone.service.UserService;
+import com.example.instagramclone.web.dto.user.UserProfileDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -16,15 +17,16 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/user/{id}")
-    public String profile(@PathVariable int id, Model model) {
-        User user = userService.userProfile(id);
-        model.addAttribute("user", user);
+    @GetMapping("/user/{pageUserId}")
+    public String profile(@PathVariable int pageUserId, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        UserProfileDto dto = userService.userProfile(pageUserId, principalDetails.getUser().getId());
+        model.addAttribute("dto", dto);
         return "user/profile";
     }
 
     @GetMapping("/user/{id}/update")
     public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
         return "user/update";
     }
 }
