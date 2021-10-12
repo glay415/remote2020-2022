d988924af944f11327e12780b27f19f000affc8c
이준서 <ljchan71@gmail.com>
2021-10-12 22:50:06 +0900

    add::SubscribeService
    subscribeList/nativeQuery

--- src/main/java/com/example/instagramclone/service/SubscribeService.java
+++ src/main/java/com/example/instagramclone/service/SubscribeService.java
@@ -1,17 +1,46 @@
 package com.example.instagramclone.service;
 
-import com.example.instagramclone.domain.subscribe.Subscribe;
 import com.example.instagramclone.domain.subscribe.SubscribeRepository;
 import com.example.instagramclone.handler.ex.CustomApiException;
+import com.example.instagramclone.web.dto.subscribe.SubscribeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import org.qlrm.mapper.JpaResultMapper;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class SubscribeService {
 
     private final SubscribeRepository subscribeRepository;
+    private final EntityManager em; //Repository 는 얘의 구현체
+
+    @Transactional(readOnly = true)
+    public List<SubscribeDto> subscribeList(int principalId, int pageUserId) {
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+        sb.append("if ((SELECT 1 FROM subscribe WHERE fromUserId = ? AND toUserId = u.id), 1, 0) subscribeState, ");
+        sb.append("if ((?=u.id), 1, 0) equalUserState ");
+        sb.append("FROM user u INNER JOIN subscribe s ");
+        sb.append("ON u.id = s.toUserId ");
+        sb.append("WHERE s.fromUserId = ?");
+
+        Query query = em.createNativeQuery(sb.toString())
+                .setParameter(1, principalId)
+                .setParameter(2, principalId)
+                .setParameter(3, pageUserId);
+
+         JpaResultMapper result = new JpaResultMapper();
+         List<SubscribeDto> subscribeDtos = result.list(query, SubscribeDto.class);
+        return subscribeDtos;
+    }
+
 
     @Transactional
     public void subscribe(int fromUserId, int toUserId) {
