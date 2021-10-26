92f22bc0bcf068400437768b19b6a29564fe6afa
이준서 <ljchan71@gmail.com>
2021-10-26 20:02:33 +0900

    start::pratice-reactive-jwt

---
+++ back/practice-reative-jwt/src/main/java/com/example/practicereativejwt/PracticeReativeJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicereativejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeReativeJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeReativeJwtApplication.class, args);
+    }
+
+}
---
+++ back/practice-reative-jwt/src/test/java/com/example/practicereativejwt/PracticeReativeJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicereativejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeReativeJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
