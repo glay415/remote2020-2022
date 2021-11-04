c464b0356a2fbe550607d86118bd8ccee316998c
이준서 <ljchan71@gmail.com>
2021-11-04 19:11:19 +0900

    🎠 :: 1-1.base-setting

---
+++ code/springboot/data-jpa/src/main/java/inflearn/datajpa/entity/Member.java
@@ -0,0 +1,42 @@
+package inflearn.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
---
+++ code/springboot/data-jpa/src/test/java/inflearn/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package inflearn.datajpa.repository;
+
+import inflearn.datajpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId().equals(member.getId()));
+        assertThat(findMember.getUsername().equals(member.getUsername()));
+        assertThat(findMember).isEqualTo(member);
+    }
+}
