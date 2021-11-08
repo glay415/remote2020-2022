cb03ccd65e9f833d84db74c96682f806bd4a4393
이준서 <ljchan71@gmail.com>
2021-11-09 08:24:41 +0900

    🎠 :: 1-12.pure-jpa-auditing

--- code/springboot/data-jpa/src/test/java/inflearn/datajpa/entity/MemberTest.java
+++ code/springboot/data-jpa/src/test/java/inflearn/datajpa/entity/MemberTest.java
@@ -1,6 +1,8 @@
 package inflearn.datajpa.entity;
 
+import inflearn.datajpa.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
@@ -16,6 +18,9 @@ public class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -44,5 +49,22 @@ public class MemberTest {
             System.out.println("member = " + member);
             System.out.println("-> member.team" + member.getTeam());
         }
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        Member member = new Member("member1");
+        memberRepository.save(member);
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();;
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        System.out.println("findMember.getCreatedDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.getUpdatedDate = " + findMember.getUpdatedDate());
     }
 }
