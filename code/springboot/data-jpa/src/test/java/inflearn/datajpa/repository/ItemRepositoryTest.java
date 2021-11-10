937a7c3b6999e60f2423bd30c25bb5a5e7d78004
이준서 <ljchan71@gmail.com>
2021-11-11 00:02:31 +0900

    🎠 :: 1-15.entity-is-new?

---
+++ code/springboot/data-jpa/src/test/java/inflearn/datajpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,18 @@
+package inflearn.datajpa.repository;
+
+import inflearn.datajpa.entity.Item;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ItemRepositoryTest {
+
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    public void save() {
+        Item item = new Item("A");
+        itemRepository.save(item);
+    }
+}
