79310d8d11cc794554c1ed065bad04e610601295
이준서 <ljchan71@gmail.com>
2021-11-11 01:07:31 +0900

    🎠 :: 1-18.projections-interface

---
+++ code/springboot/data-jpa/src/main/java/inflearn/datajpa/repository/UsernameOnly.java
@@ -0,0 +1,9 @@
+package inflearn.datajpa.repository;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public interface UsernameOnly {
+
+//    @Value("#{target.username + ' ' + target.age}")
+    String getUsername();
+}
