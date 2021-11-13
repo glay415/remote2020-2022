a6201d7933bb9ef0251afe5fd5f8b6076437924c
이준서 <ljchan71@gmail.com>
2021-11-14 01:29:01 +0900

    🎪 :: 2.1.3/findByEmail

--- spring-boot/practive-email/src/main/java/authentication/practiveemail/entity/AuthCodeRepository.java
+++ spring-boot/practive-email/src/main/java/authentication/practiveemail/entity/AuthCodeRepository.java
@@ -3,4 +3,5 @@ package authentication.practiveemail.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthCodeRepository extends JpaRepository<AuthCode, String> {
+    AuthCode findByEmail(String email);
 }
