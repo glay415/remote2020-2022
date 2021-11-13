2e7ff8bd44be4b485c86eb43502af9c6fbc1d551
이준서 <ljchan71@gmail.com>
2021-11-13 23:56:18 +0900

    ⚙️ :: 2.1.practice-email-authentication

---
+++ spring-boot/practive-email/src/test/java/authentication/practiveemail/PractiveEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package authentication.practiveemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiveEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
