9478bea8f45e68e056374fc1a2f4f406298aa48d
이준서 <ljchan71@gmail.com>
2021-11-23 12:00:48 +0900

    🎠 :: 2.3.1/base-setting

--- spring-boot/practice-reative-jwt/src/test/java/com/example/practicereactivejwt/PracticeReactiveJwtApplicationTests.java
+++ spring-boot/practice-reative-jwt/src/test/java/com/example/practicereactivejwt/PracticeReactiveJwtApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.practicereativejwt;
+package com.example.practicereactivejwt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PracticeReativeJwtApplicationTests {
+class PracticeReactiveJwtApplicationTests {
 
     @Test
     void contextLoads() {
