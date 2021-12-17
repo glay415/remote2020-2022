050fa95ed101ecdf4a5d2a0c66e26a10642b66f5
세현 <lyutvs@gmail.com>
2021-12-17 20:38:12 +0900

    📑 :: 프로젝트 설정

---
+++ src/main/java/com/example/appjambackend/AppjamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.appjambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppjamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppjamBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appjambackend/AppjamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appjambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppjamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
