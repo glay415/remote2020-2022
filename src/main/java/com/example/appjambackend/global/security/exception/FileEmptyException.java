d96a7354ccda6d37020ebc7ba8002258338b072f
세현 <lyutvs@gmail.com>
2021-12-18 18:03:17 +0900

    📑 :: FileEmptyException

---
+++ src/main/java/com/example/appjambackend/global/security/exception/FileEmptyException.java
@@ -0,0 +1,14 @@
+package com.example.appjambackend.global.security.exception;
+
+import com.example.appjambackend.global.error.exception.CustomExceptoin;
+import com.example.appjambackend.global.error.exception.ErrorCode;
+
+public class FileEmptyException extends CustomExceptoin {
+
+    private static  FileEmptyException EXCEPTONI=
+            new FileEmptyException();
+
+    public FileEmptyException() {
+        super(ErrorCode.FILE_EMPTY);
+    }
+}
