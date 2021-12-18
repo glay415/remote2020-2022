2d50d35071d7a1e929c18889e0008254a2a014c2
세현 <lyutvs@gmail.com>
2021-12-18 17:27:16 +0900

    📑 :: GlobalExceptonHandler

---
+++ src/main/java/com/example/appjambackend/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.appjambackend.global.error;
+
+import com.example.appjambackend.global.error.exception.CustomExceptoin;
+import com.example.appjambackend.global.error.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomExceptoin.class)
+    public ResponseEntity<ErrorResponse> handlingCustomException(CustomExceptoin e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return new ResponseEntity<>(new ErrorResponse(errorCode.getStatusCode(), errorCode.getMessage()),
+                HttpStatus.valueOf(errorCode.getStatusCode()));
+    }
+}
