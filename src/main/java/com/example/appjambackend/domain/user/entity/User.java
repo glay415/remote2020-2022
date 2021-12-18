fd72a94f6bf6700569da7fab90b67472fd5d553a
이준서 <ljchan71@gmail.com>
2021-12-18 15:54:07 +0900

    📑 :: user엔티티

---
+++ src/main/java/com/example/appjambackend/domain/user/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.appjambackend.domain.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", unique = true)
+    private String userId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "nick_name")
+    private String nickName;
+
+}
