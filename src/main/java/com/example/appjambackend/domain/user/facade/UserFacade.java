0397d5fde48af95ee6781569da996977b0adeaaa
이준서 <ljchan71@gmail.com>
2021-12-18 20:43:16 +0900

    ⚡ :: UserFacade

--- src/main/java/com/example/appjambackend/domain/user/facade/UserFacade.java
+++ src/main/java/com/example/appjambackend/domain/user/facade/UserFacade.java
@@ -1,7 +1,10 @@
 package com.example.appjambackend.domain.user.facade;
 
+import com.example.appjambackend.domain.user.entity.User;
 import com.example.appjambackend.domain.user.entity.UserRepository;
 import com.example.appjambackend.domain.user.exception.UseridAlreadyExistException;
+import com.example.appjambackend.global.security.details.CustomUserDetails;
+import com.example.appjambackend.global.util.user.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +13,16 @@ import org.springframework.stereotype.Component;
 public class UserFacade {
 
     private final UserRepository userRepository;
+    private final UserUtil userUtil;
 
     public void existsUseridFilter(String userid) {
         if (userRepository.findByUserid(userid).isPresent())
             throw UseridAlreadyExistException.EXCEPTION;
+    }
+
+    public User getCurrentStudent() {
+        Object principal = userUtil.getPrincipal();
+
+        return ((CustomUserDetails) principal).getUser();
     }
 }
