32b8497888e878e740fc3d82b99db347f9c40b23
세현 <lyutvs@gmail.com>
2021-12-18 18:05:26 +0900

    📑 :: FileUploadFailException

---
+++ src/main/java/com/example/appjambackend/global/security/exception/FileUploadFailException.java
@@ -0,0 +1,14 @@
+package com.example.appjambackend.global.security.exception;
+
+import com.example.appjambackend.global.error.exception.CustomExceptoin;
+import com.example.appjambackend.global.error.exception.ErrorCode;
+
+public class FileUploadFailException extends CustomExceptoin {
+
+    public static FileUploadFailException EXCEPTION =
+            new FileUploadFailException();
+
+    public FileUploadFailException() {
+        super(ErrorCode.FILE_UPLOAD_FAIL);
+    }
+}
