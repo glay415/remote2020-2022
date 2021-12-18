f3afc1ac2b38f4df3e67587d3cfb56feeb02c2f7
세현 <lyutvs@gmail.com>
2021-12-18 19:37:37 +0900

    ⚡ :: FeedServiceImpl

--- src/main/java/com/example/appjambackend/domain/feed/service/FeedServiceImpl.java
+++ src/main/java/com/example/appjambackend/domain/feed/service/FeedServiceImpl.java
@@ -1,10 +1,17 @@
 package com.example.appjambackend.domain.feed.service;
 
 import com.example.appjambackend.domain.feed.entity.Feed;
-import com.example.appjambackend.domain.feed.entity.repository.FeedRepository;
+import com.example.appjambackend.domain.feed.entity.FeedMedium;
+import com.example.appjambackend.domain.feed.entity.FeedMediumRepository;
+import com.example.appjambackend.domain.feed.entity.FeedRepository;
+import com.example.appjambackend.domain.feed.exceptoin.FeedNotFoundException;
 import com.example.appjambackend.domain.feed.facade.FeedFacade;
-import com.example.appjambackend.domain.feed.presentation.requset.ModifyFeedRequest;
-import com.example.appjambackend.domain.feed.presentation.requset.PostFeedRequest;
+import com.example.appjambackend.domain.feed.presentation.dto.requset.ModifyFeedRequest;
+import com.example.appjambackend.domain.feed.presentation.dto.requset.PostFeedRequest;
+import com.example.appjambackend.domain.user.entity.UserRepository;
+import com.example.appjambackend.global.security.exception.FileEmptyException;
+import com.example.appjambackend.global.security.exception.TooManyFilesException;
+import com.example.appjambackend.global.util.s3.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,7 +22,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FeedServiceImpl implements FeedService{
 
+    private static final int MAX_FILE_COUNT = 20;
+
     private final FeedRepository feedRepository;
+    private final UserRepository userRepository;
+    private final FeedMediumRepository feedMediumRepository;
+    private final S3Service s3Service;
     private final FeedFacade feedFacade;
 
     @Override
@@ -47,6 +59,23 @@ public class FeedServiceImpl implements FeedService{
 
     @Override
     public void uploadPhoto(List<MultipartFile> files, Long feedId) {
+
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
+
+        if(files == null)
+            throw new FileEmptyException();
+
+        if(files.size() + feedMediumRepository.countByFeed(feed) > MAX_FILE_COUNT)
+            throw new TooManyFilesException();
+
+
+        files.forEach(file ->
+                feedMediumRepository.save(FeedMedium.builder()
+                        .feed(feed)
+                        .fileName(s3Service.upload(file))
+                        .build())
+        );
 
     }
 }
