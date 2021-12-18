85eba6599257e1be35ac03554e10c8f6df81066f
세현 <lyutvs@gmail.com>
2021-12-18 16:56:52 +0900

    📑 :: ModifyFeedRequest

--- src/main/java/com/example/appjambackend/domain/feed/presentation/requset/ModifyFeedRequest.java
+++ src/main/java/com/example/appjambackend/domain/feed/presentation/requset/ModifyFeedRequest.java
@@ -5,19 +5,19 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-public class PostFeedRequest {
+public class ModifyFeedRequest {
 
-    @NotBlank(message = "title은 비어있으면 안됩니다.")
+    @NotNull
+    private Long feedId;
+
+    @NotBlank(message = "title은 공백이면 안됩니다.")
     private String title;
 
-    @NotBlank(message = "description은 비어있으면 안됩니다.")
-    private String description;
-
     @DateTimeFormat(pattern = "YYYY-MM-DD'T'hh:mm:ss")
-    private LocalDateTime createTime;
-
+    private LocalDateTime modifyTime;
 }
