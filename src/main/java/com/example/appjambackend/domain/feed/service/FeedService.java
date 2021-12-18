19c2acbd9cbb4051106dae3d394579023baba1ca
세현 <lyutvs@gmail.com>
2021-12-18 19:04:43 +0900

    ⚡ :: uploadPhoto

--- src/main/java/com/example/appjambackend/domain/feed/service/FeedService.java
+++ src/main/java/com/example/appjambackend/domain/feed/service/FeedService.java
@@ -2,6 +2,10 @@ package com.example.appjambackend.domain.feed.service;
 
 import com.example.appjambackend.domain.feed.presentation.requset.ModifyFeedRequest;
 import com.example.appjambackend.domain.feed.presentation.requset.PostFeedRequest;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface FeedService {
 
@@ -10,5 +14,6 @@ public interface FeedService {
     void modifyFeed(ModifyFeedRequest request);
 
     void removeFeed(Long id);
-    
+
+    void uploadPhoto(List<MultipartFile> files, Long feedId);
 }
