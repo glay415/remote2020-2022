f065e507a767f3e01d8e5865ab0a38e52684e2d3
이준서 <ljchan71@gmail.com>
2021-12-18 16:23:47 +0900

    📑 :: SecurityConfig

---
+++ src/main/java/com/example/appjambackend/global/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.appjambackend.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().and()
+                .formLogin().disable();
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+}
