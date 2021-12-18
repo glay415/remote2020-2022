6cca47c9a4fa1167da3ad2a98598bbc036aa3dd2
이준서 <ljchan71@gmail.com>
2021-12-18 22:01:00 +0900

    📑 :: RemoveMarkService

---
+++ src/main/java/com/example/appjambackend/domain/mark/service/RemoveMarkService.java
@@ -0,0 +1,17 @@
+package com.example.appjambackend.domain.mark.service;
+
+import com.example.appjambackend.domain.mark.entity.Mark;
+import com.example.appjambackend.domain.mark.entity.MarkRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RemoveMarkService {
+
+    private final MarkRepository markRepository;
+
+    public void execute(Long feedId) {
+        markRepository.deleteById(feedId);
+    }
+}
