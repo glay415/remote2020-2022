ad4b40bdfae62428c2ba5bdf9fe5b52bf3ef60a5
세현 <lyutvs@gmail.com>
2021-12-18 19:36:49 +0900

    ⚡ :: FeedFacade

--- src/main/java/com/example/appjambackend/domain/feed/facade/FeedFacade.java
+++ src/main/java/com/example/appjambackend/domain/feed/facade/FeedFacade.java
@@ -1,7 +1,7 @@
 package com.example.appjambackend.domain.feed.facade;
 
 import com.example.appjambackend.domain.feed.entity.Feed;
-import com.example.appjambackend.domain.feed.entity.repository.FeedRepository;
+import com.example.appjambackend.domain.feed.entity.FeedRepository;
 import com.example.appjambackend.domain.feed.exceptoin.FeedNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
