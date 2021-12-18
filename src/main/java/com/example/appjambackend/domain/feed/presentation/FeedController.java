b107d7c4cb8823d8d19881aa4903a249cc3d7f89
세현 <lyutvs@gmail.com>
2021-12-18 19:13:54 +0900

    ⚡ :: /{feed_id}/photo

--- src/main/java/com/example/appjambackend/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/appjambackend/domain/feed/presentation/FeedController.java
@@ -1,2 +1,23 @@
-package com.example.appjambackend.domain.feed.presentation;public class FeedController {
+package com.example.appjambackend.domain.feed.presentation;
+
+import com.example.appjambackend.domain.feed.service.FeedService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/feed")
+@RequiredArgsConstructor
+public class FeedController {
+
+    private final FeedService feedService;
+
+    @PostMapping("/{feed_id}/photo")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void uploadPhoto(@RequestPart List<MultipartFile> files, @PathVariable("feed_id") Long feedId) {
+        feedService.uploadPhoto(files, feedId);
+    }
 }
