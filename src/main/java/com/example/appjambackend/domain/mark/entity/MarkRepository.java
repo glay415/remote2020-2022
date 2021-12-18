25cb7732f4024ff82cdd5ad7fa2e803f72bf3d60
이준서 <ljchan71@gmail.com>
2021-12-18 22:00:33 +0900

    📑 :: MarkEntity

---
+++ src/main/java/com/example/appjambackend/domain/mark/entity/MarkRepository.java
@@ -0,0 +1,6 @@
+package com.example.appjambackend.domain.mark.entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarkRepository extends CrudRepository<Mark, Long> {
+}
