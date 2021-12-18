75970d74dc1e05306a4414bf80504cab32dd42fe
세현 <lyutvs@gmail.com>
2021-12-18 19:45:53 +0900

    ⚡ :: FeedController

--- src/main/java/com/example/appjambackend/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/appjambackend/domain/feed/presentation/FeedController.java
@@ -1,11 +1,15 @@
 package com.example.appjambackend.domain.feed.presentation;
 
+import com.example.appjambackend.domain.feed.presentation.dto.requset.ModifyFeedRequest;
+import com.example.appjambackend.domain.feed.presentation.dto.requset.PostFeedRequest;
+import com.example.appjambackend.domain.feed.presentation.dto.response.PostFeedResponse;
 import com.example.appjambackend.domain.feed.service.FeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,7 +19,17 @@ public class FeedController {
 
     private final FeedService feedService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PostFeedResponse feedResponse(@RequestBody @Valid PostFeedRequest request) {
+        return feedService.postFeed(request);
+    }
 
+    @PatchMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void modifyCarrotFeed(@RequestBody @Valid ModifyFeedRequest request) {
+        feedService.modifyFeed(request);
+    }
 
     @PostMapping("/{feed_id}/photo")
     @ResponseStatus(HttpStatus.CREATED)
