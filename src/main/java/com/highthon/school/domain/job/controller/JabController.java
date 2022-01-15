983cb7f9557c7aad7982486223c4746c6cf33c2d
joungeun <lje0818ovo@naver.com>
2022-01-15 17:59:31 +0900

    [ADD] Jab list controller

--- src/main/java/com/highthon/school/domain/job/controller/JabController.java
+++ src/main/java/com/highthon/school/domain/job/controller/JabController.java
@@ -1,10 +1,16 @@
 package com.highthon.school.domain.job.controller;
 
+import com.highthon.school.domain.job.Branch;
 import com.highthon.school.domain.job.dto.CreateJabRequestDto;
+import com.highthon.school.domain.job.dto.JabInfoResponseDto;
 import com.highthon.school.domain.job.service.JabService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,7 +18,12 @@ public class JabController {
     private final JabService jabService;
 
     @PostMapping("/jab")
-    public void CreateJab(CreateJabRequestDto req){
+    public void createJab(CreateJabRequestDto req){
         jabService.createJab(req);
+    }
+
+    @GetMapping("/jab-list/{branch}")
+    public List<JabInfoResponseDto> jabList(@PathVariable Branch branch){
+        return jabService.jabList(branch, "token");
     }
 }
