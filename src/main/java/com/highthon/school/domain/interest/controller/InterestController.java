fd7b284cc8fe9b3962d6a9b31ede763d968cdb19
joungeun <lje0818ovo@naver.com>
2022-01-15 20:27:06 +0900

    [ADD] Get Interest jab API

--- src/main/java/com/highthon/school/domain/interest/controller/InterestController.java
+++ src/main/java/com/highthon/school/domain/interest/controller/InterestController.java
@@ -1,10 +1,14 @@
 package com.highthon.school.domain.interest.controller;
 
+import com.highthon.school.domain.interest.dto.InterestJabResponseDto;
 import com.highthon.school.domain.interest.service.InterestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,5 +18,10 @@ public class InterestController {
     @PostMapping("/interest/{name}")
     public void interestJab(@PathVariable(name = "name") String jabName){
         interestService.interestJab(jabName);
+    }
+
+    @GetMapping("/interest")
+    public List<InterestJabResponseDto> getMyInterestJab(){
+        return interestService.getInterestJab();
     }
 }
