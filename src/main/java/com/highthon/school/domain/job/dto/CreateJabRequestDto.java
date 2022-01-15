8860265500f74d9ff21746d9e0a2bcdf48017299
joungeun <lje0818ovo@naver.com>
2022-01-15 22:14:53 +0900

    [FIX] @AllArgsConstructor -> @NoArgsConstructor

--- src/main/java/com/highthon/school/domain/job/dto/CreateJabRequestDto.java
+++ src/main/java/com/highthon/school/domain/job/dto/CreateJabRequestDto.java
@@ -1,13 +1,13 @@
 package com.highthon.school.domain.job.dto;
 
 import com.highthon.school.domain.job.Branch;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class CreateJabRequestDto {
-    private final String name;
-    private final Branch branch;
-    private final String intro;
+    private String name;
+    private Branch branch;
+    private String intro;
 }
