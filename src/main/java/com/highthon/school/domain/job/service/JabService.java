00a68fedc8ccd4589928792a2d3d749fc9be9214
joungeun <lje0818ovo@naver.com>
2022-01-15 18:00:38 +0900

    [ADD] JabList method

--- src/main/java/com/highthon/school/domain/job/service/JabService.java
+++ src/main/java/com/highthon/school/domain/job/service/JabService.java
@@ -1,10 +1,18 @@
 package com.highthon.school.domain.job.service;
 
+import com.highthon.school.domain.interest.Interest;
+import com.highthon.school.domain.interest.repository.InterestRepository;
+import com.highthon.school.domain.job.Branch;
 import com.highthon.school.domain.job.Jab;
 import com.highthon.school.domain.job.dto.CreateJabRequestDto;
+import com.highthon.school.domain.job.dto.JabInfoResponseDto;
 import com.highthon.school.domain.job.repository.JabRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +28,25 @@ public class JabService {
                         .branch(req.getBranch())
                         .build()
         );
+    }
+
+    public List<JabInfoResponseDto> jabList(Branch branch, String userId){
+        List<Jab> jabs = jabRepository.findAllByBranch(branch);
+        List<JabInfoResponseDto> res = new ArrayList<>();
+        for(Jab jab : jabs){
+            res.add(
+                    JabInfoResponseDto.builder()
+                                    .jab(jab.getName())
+                                    .intro(jab.getIntro())
+                                    .interest(interestCheck(userId, jab.getName()))
+                                    .branch(jab.getBranch()).build()
+            );
+        }
+        return res;
+    }
+
+    private boolean interestCheck(String userId, String jabName){
+        Optional<Interest> interest = interestRepository.findByJab_NameAndUser_Id(jabName, userId);
+        return interest.isPresent();
     }
 }
