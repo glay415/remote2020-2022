beaa304f1b40d964bb68459e25b238dc2d07fb6a
joungeun <lje0818ovo@naver.com>
2022-01-15 16:51:52 +0900

    [ADD] CreateJab method to JabService

---
+++ src/main/java/com/highthon/school/domain/job/service/JabService.java
@@ -0,0 +1,19 @@
+package com.highthon.school.domain.job.service;
+
+import com.highthon.school.domain.job.Jab;
+import com.highthon.school.domain.job.dto.CreateJabRequestDto;
+import com.highthon.school.domain.job.repository.JabRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JabService {
+    private final JabRepository jabRepository;
+    public void createJab(CreateJabRequestDto req){
+        jabRepository.save(
+                Jab.builder()
+                        .name(req.getName())
+                        .branch(req.getBranch())
+                        .build()
+        );
+    }
+}
