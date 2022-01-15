774302d963938adb2280dd679b1b92f5222e68fa
joungeun <lje0818ovo@naver.com>
2022-01-15 16:52:46 +0900

    [ADD] Branch to Jab

--- src/main/java/com/highthon/school/domain/job/Jab.java
+++ src/main/java/com/highthon/school/domain/job/Jab.java
@@ -1,18 +1,27 @@
 package com.highthon.school.domain.job;
 
 import com.highthon.school.domain.board.Board;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.List;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Jab {
     @Id
     private String name;
 
+    private Branch branch;
+
     @OneToMany
     private List<Board> boards;
-    // 카테고리 이넘
 }
