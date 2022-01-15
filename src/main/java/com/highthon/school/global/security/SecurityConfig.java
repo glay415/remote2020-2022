c35e14b4d180d3536612dfd7e7f3d027436d38fa
이준서 <ljchan71@gmail.com>
2022-01-15 18:08:39 +0900

    [UPDATE] SecurityConfig

--- src/main/java/com/highthon/school/global/security/SecurityConfig.java
+++ src/main/java/com/highthon/school/global/security/SecurityConfig.java
@@ -1,14 +1,28 @@
 package com.highthon.school.global.security;
 
+import com.highthon.school.global.security.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final JwtFilter jwtFilter;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -23,6 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 			.and()
 			.authorizeRequests()
-			.anyRequest().permitAll();
+			.anyRequest().permitAll()
+
+			.and()
+			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
