c750d7208ac81062a630d81267cb827dbba34a0a
이준서 <ljchan71@gmail.com>
2022-01-16 01:14:00 +0900

    [ADD] Comment Reply

---
+++ src/main/java/com/highthon/school/domain/comment/dto/CommentResponseDto.java
@@ -0,0 +1,10 @@
+package com.highthon.school.domain.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommentResponseDto {
+	private final Integer commentId;
+}
