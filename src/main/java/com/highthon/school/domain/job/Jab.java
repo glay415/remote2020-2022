0a5ace31bb5cb33210108466c98c3909305cf273
joungeun <lje0818ovo@naver.com>
2022-01-15 16:28:07 +0900

    [ADD] Relationship with interest

--- src/main/java/com/highthon/school/domain/job/Jab.java
+++ src/main/java/com/highthon/school/domain/job/Jab.java
@@ -1,12 +1,18 @@
 package com.highthon.school.domain.job;
 
+import com.highthon.school.domain.board.Board;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Jab {
     @Id
     private String name;
+
+    @OneToMany
+    private List<Board> boards;
     // 카테고리 이넘
-    // 관심 관계
 }
--- src/main/java/com/highthon/school/domain/user/User.java
+++ src/main/java/com/highthon/school/domain/user/User.java
@@ -1,7 +1,11 @@
 package com.highthon.school.domain.user;
 
+import com.highthon.school.domain.interest.Interest;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class User {
@@ -9,4 +13,7 @@ public class User {
     private String id;
     private String password;
     private String nickname;
+
+    @OneToMany
+    private List<Interest> interest;
 }
