4b159c54b0cd8756ada864a22ec914965e2b0b20
joungeun <lje0818ovo@naver.com>
2022-01-15 19:30:48 +0900

    [FIX] Add @Component to Filter

--- src/main/java/com/highthon/school/global/security/jwt/JwtFilter.java
+++ src/main/java/com/highthon/school/global/security/jwt/JwtFilter.java
@@ -8,8 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
