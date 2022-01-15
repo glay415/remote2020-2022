10612f59fa145b4ea49b61f148ce3b272a9a506c
이준서 <ljchan71@gmail.com>
2022-01-16 01:15:29 +0900

    [ADD] Reply Api

---
+++ src/main/java/com/highthon/school/domain/reply/dto/ReplyResponseDto.java
@@ -0,0 +1,10 @@
+package com.highthon.school.domain.reply.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReplyResponseDto {
+	private final Integer replyId;
+}
