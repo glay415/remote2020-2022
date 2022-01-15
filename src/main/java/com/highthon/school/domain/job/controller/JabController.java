1acdc8798adf566ab9d51b0c79dabb34aa1b3690
joungeun <lje0818ovo@naver.com>
2022-01-15 19:01:56 +0900

    [ADD] Get jab details API

--- src/main/java/com/highthon/school/domain/job/controller/JabController.java
+++ src/main/java/com/highthon/school/domain/job/controller/JabController.java
@@ -5,10 +5,7 @@ import com.highthon.school.domain.job.dto.CreateJabRequestDto;
 import com.highthon.school.domain.job.dto.JabInfoResponseDto;
 import com.highthon.school.domain.job.service.JabService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,12 +15,17 @@ public class JabController {
     private final JabService jabService;
 
     @PostMapping("/jab")
-    public void createJab(CreateJabRequestDto req){
+    public void createJab(@RequestBody CreateJabRequestDto req){
         jabService.createJab(req);
     }
 
     @GetMapping("/jab-list/{branch}")
     public List<JabInfoResponseDto> jabList(@PathVariable Branch branch){
         return jabService.jabList(branch, "token");
+    }
+
+    @GetMapping("/jab/{name}")
+    public JabInfoResponseDto getJab(@PathVariable String name){
+        return jabService.getJabDetails(name, "userId");
     }
 }
