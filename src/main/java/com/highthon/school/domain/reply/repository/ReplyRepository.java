53c70ea64feef47850d95e849b82d7c818878a77
이준서 <ljchan71@gmail.com>
2022-01-16 01:15:42 +0900

    [ADD] Reply Logic

---
+++ src/main/java/com/highthon/school/domain/reply/repository/ReplyRepository.java
@@ -0,0 +1,8 @@
+package com.highthon.school.domain.reply.repository;
+
+import com.highthon.school.domain.reply.Reply;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReplyRepository extends CrudRepository<Reply, Integer> {
+
+}
